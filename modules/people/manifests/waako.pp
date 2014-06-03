7c558341783fd5f0e43cc4b70a598918a7f173b0
Tom Bamford <tom@halfmesh.com>
2014-06-03 09:24:55 +0100

    added notify resource to make sure I know that my personal puppet file config is being run (taken from http://garylarizza.com/blog/2013/02/15/puppet-plus-github-equals-laptop-love/)

--- modules/people/manifests/waako.pp
+++ modules/people/manifests/waako.pp
@@ -1,11 +1,26 @@
 class people::waako {
 
+  notify { 'class people::waako declared': }
+
   $home     = "/Users/${::boxen_user}"
   $my       = "${home}/my"
   $dotfiles = "${my}/dotfiles"
   
   file { $my:
     ensure  => directory
+  }
+
+  # Changes the default shell to the zsh version we get from Homebrew
+  # Uses the osx_chsh type out of boxen/puppet-osx
+  osx_chsh { $::boxen_user:
+    shell   => '/opt/boxen/homebrew/bin/zsh',
+    require => Package['zsh'],
+  }
+
+  file_line { 'add zsh to /etc/shells':
+    path    => '/etc/shells',
+    line    => "${boxen::config::homebrewdir}/bin/zsh",
+    require => Package['zsh'],
   }
 
   repository { $dotfiles:
