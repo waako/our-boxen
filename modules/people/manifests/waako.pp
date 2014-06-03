b500285cefcf0d2bcee56a00ea1a2a27152e47e5
Tom Bamford <tom@halfmesh.com>
2014-06-03 09:19:31 +0100

    added personal puppet file

---
+++ modules/people/manifests/waako.pp
@@ -0,0 +1,15 @@
+class people::waako {
+
+  $home     = "/Users/${::boxen_user}"
+  $my       = "${home}/my"
+  $dotfiles = "${my}/dotfiles"
+  
+  file { $my:
+    ensure  => directory
+  }
+
+  repository { $dotfiles:
+    source  => 'waako/dotfiles',
+    require => File[$my]
+  }
+}
